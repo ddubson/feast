736906caff0210d2c061a8e23eeed459c87ad2fe
Dmitriy Dubson <d.dubson@gmail.com>
2019-11-08 19:53:11 -0500

    Add @testing-library/react

--- web/test/AppRoot.test.tsx
+++ web/test/AppRoot.test.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import AppRoot from "../src/AppRoot";
-import {find, renderApp, textOf} from "./helpers/RenderApp";
+import {render} from "@testing-library/react";
 
 describe("AppRoot", () => {
-  beforeEach(() => {
-    renderApp();
-  });
-
   it("should display the app title", () => {
-    expect(textOf(find("[data-test='app-title']"))).toEqual("Feast");
+    const { getByText } = render(<AppRoot />);
+    expect(getByText("Feast")).toBeTruthy();
   });
 });
