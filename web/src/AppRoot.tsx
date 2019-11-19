39a1397a93ea881592faf8f7460c4f4ea160ce9b
Dmitriy Dubson <d.dubson@gmail.com>
2019-11-19 07:21:10 -0500

    Create simple formik form

--- web/src/AppRoot.tsx
+++ web/src/AppRoot.tsx
@@ -1,7 +1,7 @@
 import React, {PureComponent} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Container, Menu} from "semantic-ui-react";
-import {diContainer, DIContainerContext} from "./AppConfig";
+import {DIContainerContext} from "./AppConfig";
 import CreateRecipeScene from "./recipes/scenes/CreateRecipeScene";
 import RecipeOverviewScene from "./recipes/scenes/RecipeOverviewScene";
 import RecipesDashboardScene from "./recipes/scenes/RecipesDashboardScene";
@@ -29,7 +29,7 @@ export default class AppRoot extends PureComponent {
                   <Route path={"/"} exact={true} render={() =>
                     <RecipesDashboardScene recipesService={recipesService}/>}/>
                   <Route path={"/create-recipe"} exact={true}
-                         render={({history}) => <CreateRecipeScene history={history}/>}/>
+                         render={({history}) => <CreateRecipeScene recipesService={recipesService} history={history}/>}/>
                   <Route path={"/recipe/:id"} render={(props) => <RecipeOverviewScene {...props} />}/>
                 </Switch>
               </Container>
--- web/src/recipes/scenes/CreateRecipeScene.tsx
+++ web/src/recipes/scenes/CreateRecipeScene.tsx
@@ -1,11 +1,10 @@
-import update from "immutability-helper";
-import React, {ChangeEvent, FormEvent, PureComponent} from "react";
-import * as shortid from "shortid";
-import {DIContainerContext} from "../../AppConfig";
-import {Ingredient, Recipe} from "../../application/types";
-import {BackToRecipesLink} from "../components/BackToRecipesLink";
+import React from "react";
+import {Ingredient} from "../../application/types";
+import {useFormik} from "formik";
+import {RecipesService} from "../../application/services/RecipesService";
 
 export interface CreateRecipeSceneProps {
+  recipesService: RecipesService;
   history: {
     push: (location: string) => void;
   };
@@ -23,124 +22,30 @@ export interface CreateRecipeSceneState {
   };
 }
 
-class CreateRecipeScene extends PureComponent<CreateRecipeSceneProps, CreateRecipeSceneState> {
-  public static contextType = DIContainerContext;
-  public context!: React.ContextType<typeof DIContainerContext>;
-
-  constructor(props: CreateRecipeSceneProps) {
-    super(props);
-    this.state = {
-      ingredientToAdd: {
-        form: "",
-        name: "",
-        quantity: 0,
-      },
-      recipeForm: {
-        ingredients: [],
-        name: "",
-      },
-    };
-    this.addRecipeName = this.addRecipeName.bind(this);
-    this.addIngredientQuantity = this.addIngredientQuantity.bind(this);
-    this.addIngredientForm = this.addIngredientForm.bind(this);
-    this.addIngredientName = this.addIngredientName.bind(this);
-    this.addIngredientToIngredientList = this.addIngredientToIngredientList.bind(this);
-  }
-
-  public render() {
-    const {recipeForm, ingredientToAdd} = this.state;
-
-    return (<div>
-      <BackToRecipesLink/>
-
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <input
-          placeholder="Recipe Name"
-          value={this.state.recipeForm.name}
-          onChange={this.addRecipeName}
-        />
-
-        <div className="ingredient-list">
-          <div>Ingredients</div>
-          <div>
-            {(recipeForm.ingredients.length > 0 ?
-              recipeForm.ingredients.map((ingredient: Ingredient) => (
-                <div key={shortid.generate()}>{ingredient.quantity} {ingredient.name}</div>
-              )) : "No ingredients yet.")}
-          </div>
-          <div>
-            <div>Add Ingredient</div>
-            <div>
-              <input data-ingredient-quantity
-                     type="number"
-                     value={ingredientToAdd.quantity}
-                     onChange={this.addIngredientQuantity}
-                     name="ingredientQuantity"
-                     min={0}
-                     max={100}
-                     placeholder="Quantity"/>
-              <input data-ingredient-form
-                     type="text"
-                     value={ingredientToAdd.form}
-                     onChange={this.addIngredientForm}
-                     name="ingredientForm"
-                     placeholder="Form"/>
-              <input data-ingredient-name
-                     type="text"
-                     value={ingredientToAdd.name}
-                     onChange={this.addIngredientName}
-                     name="ingredientName"
-                     placeholder="Ingredient"/>
-              <button data-add-ingredient color="primary" onClick={this.addIngredientToIngredientList}>
-                Add Ingredient
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <input data-add-recipe type="submit" value={"Create recipe"}/>
-      </form>
-    </div>);
-  }
-
-  private addRecipeName(event: ChangeEvent<HTMLInputElement>): void {
-    this.setState(update(this.state, {recipeForm: {name: {$set: event.target.value}}}));
-  }
-
-  private addIngredientQuantity(event: ChangeEvent<HTMLInputElement>): void {
-    this.setState(update(this.state, {ingredientToAdd: {quantity: {$set: parseInt(event.target.value, 10)}}}));
-  }
-
-  private addIngredientForm(event: ChangeEvent<HTMLInputElement>): void {
-    this.setState(update(this.state, {ingredientToAdd: {form: {$set: event.target.value}}}));
-  }
-
-  private addIngredientName(event: ChangeEvent<HTMLInputElement>): void {
-    this.setState(update(this.state, {ingredientToAdd: {name: {$set: event.target.value}}}));
-  }
-
-  private addIngredientToIngredientList(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    const {name, form, quantity} = this.state.ingredientToAdd;
-    const ingredient: Ingredient = {name, form, quantity};
-    this.setState(update(this.state, {recipeForm: {ingredients: {$push: [ingredient]}}}));
-  }
-
-  private handleSubmit(event: FormEvent): void {
-    event.preventDefault();
-
-    const {name, ingredients} = this.state.recipeForm;
-
-    const recipe: Recipe = {
-      id: shortid.generate(),
-      ingredients,
-      name,
-    };
-
-    this.context.recipesGateway.saveRecipe(recipe).then(() => {
-      this.props.history.push("/");
-    });
-  }
+interface RecipeForm {
+  name: string;
 }
+
+function CreateRecipeScene(props: CreateRecipeSceneProps) {
+  const formik = useFormik<RecipeForm>({
+    initialValues: {
+      name: "",
+    },
+    onSubmit: (values: RecipeForm) => {
+      console.log("submitted: ", values);
+    },
+  });
+
+  return (
+    <form onSubmit={(e) => { e.preventDefault(); formik.handleSubmit(e); }}>
+      <label htmlFor={name}>Recipe Name</label>
+      <input type="text" id="name"
+             onChange={formik.handleChange}
+             value={formik.values.name}/>
+
+      <button type={"submit"}>Submit</button>
+    </form>
+  );
+};
 
 export default CreateRecipeScene;
