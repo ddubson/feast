c7e96cda8b08a0ab76ec44f6001886462c282a2b
Dmitriy Dubson <d.dubson@gmail.com>
2020-03-31 19:44:22 -0400

    Add more styling to login page

--- src/scenes/login/LoginScene.tsx
+++ src/scenes/login/LoginScene.tsx
@@ -6,9 +6,13 @@ export const LoginScene: React.FC = () => {
   const {loginWithRedirect} = useAuth0();
 
   return (
-    <div className="ui container">
-      <div className="ui section">
-        <button className="ui primary button" onClick={() => loginWithRedirect({})}>Log in</button>
+    <div className="ui container segment mtxxl">
+      <div className="row">
+        <h1 className="ui header center aligned blue">Feast</h1>
+        <h3 className="ui header center aligned">A place for recipes.</h3>
+      </div>
+      <div className="row center-text mtxl">
+        <button className="ui primary center floated button" onClick={() => loginWithRedirect({})}>Log in</button>
       </div>
     </div>
   );
